2d44acafc9486bc78d4de998dcd978389fb81fc6
David Erni <david.erni@4teamwork.ch>
2018-04-05 14:08:17 +0200

    Migrate submitted proposal data.

--- opengever/core/upgrades/20180404140057_poc_spv_migration/upgrade.py
+++ opengever/core/upgrades/20180404140057_poc_spv_migration/upgrade.py
@@ -14,6 +14,9 @@ MIME_DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 
 
 def sanitize_text(text):
+    """Copied from AgendaItem. IIRC sablon is a bit sensitive and does not
+    treat empty strings falsy.
+    """
     if not text:
         return None
     return text
@@ -32,7 +35,9 @@ class UpgradeSablonTemplateWrapper(object):
 
 
 class ProposalMigrator(object):
-
+    """Generate Word *.docx files for porposals in a dossier based on their
+    fields and a sablon template.
+    """
     def __init__(self, proposal_template):
         self.normalizer = getUtility(IIDNormalizer)
         self.proposal_template = proposal_template
@@ -44,8 +49,17 @@ class ProposalMigrator(object):
         }
 
         # keep agenda items list for easier template reusability
+        root['agenda_items'] = [self.get_agenda_item_data(proposal)]
+        return root
+
+    def get_agenda_item_data(self, proposal):
+        """Build data for one proposal.
+
+        We call it agenda_item here as this is the name used in the templates.
+        To make existing templates reusable for the miration we don't change
+        the data structre.
+        """
         data = {}
-        root['agenda_items'] = [data]
 
         data['title'] = proposal.title
         data['html:legal_basis'] = sanitize_text(proposal.legal_basis)
@@ -55,7 +69,7 @@ class ProposalMigrator(object):
         data['html:publish_in'] = sanitize_text(proposal.publish_in)
         data['html:disclose_to'] = sanitize_text(proposal.disclose_to)
         data['html:copy_for_attention'] = sanitize_text(proposal.copy_for_attention)
-        return root
+        return data
 
     def get_json_data(self, proposal):
         return json.dumps(self.get_data(proposal))
@@ -71,18 +85,45 @@ class ProposalMigrator(object):
                 data=sablon.file_data,
                 content_type=MIME_DOCX)
         else:
-            url = '/'.join(proposal.absolute_url())
-            msg = 'could not migrate propsal at: "{}"'.format(url)
+            url = proposal.absolute_url()
+            msg = 'Could not generate sablon document: "{}"'.format(url)
             logger.error(msg)
+            logger.error(sablon.stderr)
+
+
+class SubmittedProposalMigrator(ProposalMigrator):
+    """Generate Word *.docx files for submitted porposals in a committee based
+    on their fields and a sablon template.
+    """
+    def get_agenda_item_data(self, submitted_proposal):
+        data = super(SubmittedProposalMigrator, self).get_agenda_item_data(
+            submitted_proposal)
+
+        data['html:considerations'] = sanitize_text(
+            submitted_proposal.considerations)
+
+        model = submitted_proposal.load_model()
+        data['html:decision'] = sanitize_text(model.get_decision())
+
+        agenda_item = model.agenda_item
+        # huh, no getter there?
+        discussion = agenda_item.discussion if agenda_item else None
+        data['html:discussion'] = sanitize_text(discussion)
+
+        data['decision_number'] = model.get_decision_number()
+        data['dossier_reference_number'] = model.dossier_reference_number
+        data['repository_folder_title'] = model.repository_folder_title
+
+        return data
 
 
 class POCSPVMigration(UpgradeStep):
     """POC SPV migration.
     """
-
     def __call__(self):
         self.install_upgrade_profile()
         self.migrate_proposals()
+        self.migrate_submitted_proposals()
 
     def migrate_proposals(self):
         proposal_template = UpgradeSablonTemplateWrapper(
@@ -91,5 +132,15 @@ class POCSPVMigration(UpgradeStep):
 
         for proposal in self.objects(
                 {'portal_type': 'opengever.meeting.proposal'},
-                'Create proposal wordfiles'):
+                'Create proposal documents'):
             migrator.migrate(proposal)
+
+    def migrate_submitted_proposals(self):
+        submitted_proposal_template = UpgradeSablonTemplateWrapper(
+            templates.load('template-submittedproposal.docx'))
+        migrator = SubmittedProposalMigrator(submitted_proposal_template)
+
+        for submitted_proposal in self.objects(
+                {'portal_type': 'opengever.meeting.submittedproposal'},
+                'Create submitted proposal documents'):
+            migrator.migrate(submitted_proposal)
