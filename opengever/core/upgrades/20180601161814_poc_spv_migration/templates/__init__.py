ffb5c6737d4d8e410c57bf062e886b3135de7484
David Erni <david.erni@4teamwork.ch>
2018-06-04 14:41:56 +0200

    Touch upgrade step dir.

--- opengever/core/upgrades/20180601161814_poc_spv_migration/templates/__init__.py
+++ opengever/core/upgrades/20180601161814_poc_spv_migration/templates/__init__.py
@@ -3,5 +3,5 @@ from pkg_resources import resource_string
 
 def load(filename):
     return resource_string(
-        'opengever.core.upgrades.20180404140057_poc_spv_migration.templates',
+        'opengever.core.upgrades.20180601161814_poc_spv_migration.templates',
         filename)
